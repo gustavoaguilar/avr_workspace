558fa8e2cdf43c38645bea65d012d5f0c5fa70be
gustavoaguilar <gustavooaguilar@gmail.com>
2021-09-28 20:55:38 -0300

    Added blink and Template projects

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # avr_workspace
 Repository for studies and project using AVR MCUs 
+
+Install tools (Ubuntu):
+
+`sudo apt install gcc-avr avr-libc binutils-avr make`
---
+++ projects/blink/main.c
@@ -0,0 +1,19 @@
+#define F_CPU 1000000L
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+void main(void){
+ 
+    DDRB |= (1 << PB1);
+    PORTB |= ~(1 << PB1);
+
+    while(1){
+        PORTB |= (1 << PB1);
+        _delay_ms(500);
+        PORTB &= ~(1 << PB1);
+        _delay_ms(500);
+    }
+
+}
